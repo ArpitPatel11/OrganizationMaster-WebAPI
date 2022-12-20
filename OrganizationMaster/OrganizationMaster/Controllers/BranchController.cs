c4cca8fd5ae7f3c21cf45464c2e1196616272f5d
Admin <Admin@ARPITATOZ>
2022-12-20 17:07:14 +0530

    Add Branch Controller&Connection strings changes

---
+++ OrganizationMaster/OrganizationMaster/Controllers/BranchController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrganizationMaster.Data;
+using OrganizationMaster.Models;
+
+namespace OrganizationMaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchController : ControllerBase
+    {
+        private readonly FirstDBContext _context;
+
+        public BranchController(FirstDBContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tblbranchmaster>>> GetTblbranches()
+        {
+            if (_context.Tblbranchmasters == null)
+            {
+                return NotFound();
+            }
+            return await _context.Tblbranchmasters.ToListAsync();
+        }
+
+
+        [HttpGet("ById")]
+
+        public async Task<IEnumerable<USP_Branch_ByIdResult>> Branch_ByIdResultsAsync(int? Branch_Id)
+        {
+            return await _context.GetProcedures().USP_Branch_ByIdAsync(Branch_Id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblBranchMaster(int id, Tblbranchmaster tblbranchmaster)
+        {
+            if (id != tblbranchmaster.BranchId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblbranchmaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblorganizationmasterExists(id))
+                {
+                    return Ok(tblbranchmaster);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(tblbranchmaster);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tblbranchmaster>> PostTblBranchnmaster(Tblbranchmaster tblbranchmaster)
+        {
+            if (_context.Tblbranchmasters == null)
+            {
+                return Problem("Entity set 'FirstDBContext.Tblbranchmasters'  is null.");
+            }
+            _context.Tblbranchmasters.Add(tblbranchmaster);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTblbranches), new { id = tblbranchmaster.BranchId }, tblbranchmaster);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblBranchmaster(int id)
+        {
+            if (_context.Tblbranchmasters == null)
+            {
+                return NotFound();
+            }
+            var tblbranchmaster = await _context.Tblbranchmasters.FindAsync(id);
+            if (tblbranchmaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tblbranchmasters.Remove(tblbranchmaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool TblorganizationmasterExists(int id)
+        {
+            return (_context.Tblorganizationmasters?.Any(e => e.OraganizationId == id)).GetValueOrDefault();
+        }
+    }
+}
--- OrganizationMaster/OrganizationMaster/Models/Tblcity.cs
+++ OrganizationMaster/OrganizationMaster/Models/Tblcity.cs
@@ -8,10 +8,10 @@ namespace OrganizationMaster.Models
 {
     public partial class Tblcity
     {
-        public string CountryName { get; set; }
+        //public string CountryName { get; set; }
         [Required]
         public int? StateId { get; set; }
-        public string StateName { get; set; }
+        //public string StateName { get; set; }
         public int CityId { get; set; }
 
         [StringLength(20, ErrorMessage ="CityName is too long")]
